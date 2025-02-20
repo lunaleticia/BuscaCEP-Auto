0f867fdbb9882849831f778ecdfaaa3ae97e8a59
Letícia <leticialuna1400@gmail.com>
2025-02-20 09:55:59 -0300

    first commit

---
+++ src/cep/Sobre.java
@@ -0,0 +1,104 @@
+package cep;
+
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.EventQueue;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Sobre extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sobre dialog = new Sobre();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Sobre() {
+		setModal(true);
+		getContentPane().setForeground(SystemColor.desktop);
+		setResizable(false);
+		setTitle("Sobre");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/home.png")));
+		setBounds(150, 150, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Buscar CEP - Ver 1.0");
+		lblNewLabel.setBounds(30, 36, 316, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("@Author Letícia Luna");
+		lblNewLabel_1.setForeground(SystemColor.desktop);
+		lblNewLabel_1.setBounds(30, 83, 299, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("WEB Service: ");
+		lblNewLabel_2.setBounds(30, 129, 89, 14);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblWebService = new JLabel("republicavirtual.com.br");
+		lblWebService.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				link("https://republicavirtual.com.br/");
+			}
+		});
+		lblWebService.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblWebService.setForeground(SystemColor.textHighlight);
+		lblWebService.setBounds(116, 129, 250, 14);
+		getContentPane().add(lblWebService);
+		
+		JButton btnGithub = new JButton("");
+		btnGithub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				link("https://github.com/lunaleticia/BuscaCEP-Auto");
+			}
+		});
+		btnGithub.setToolTipText("Projeto");
+		btnGithub.setIcon(new ImageIcon(Sobre.class.getResource("/img/github.png")));
+		btnGithub.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnGithub.setBorder(null);
+		btnGithub.setBackground(SystemColor.control);
+		btnGithub.setBounds(30, 188, 45, 45);
+		getContentPane().add(btnGithub);
+
+	} // fim do construtor
+	
+	private void link(String site) {
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			URI uri = new URI(site);
+			desktop.browse(uri);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BuscarCEP {
+	requires java.desktop;
+	requires Atxy2k;
+	requires org.dom4j;
+}
